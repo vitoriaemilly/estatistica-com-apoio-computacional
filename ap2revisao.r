71abb8fa07ae9ab3e3317536aa4c8c3f73efa0c9
vitoria emilly <125896190+vitoriaemilly@users.noreply.github.com>
2026-06-08 20:03:45 -0300

    Add statistical problem statements in ap2revisao.r

---
+++ ap2revisao.r
@@ -0,0 +1,4 @@
+#De experiências passadas, sabe-se que o desvio padrão da altura de crianças da 5a série do 1o grau é 5cm.
+#a) Colhendo uma amostra de 36 dessas crianças, observou-se a média de 150cm. Qual o intervalo
+#de confiança de 95% para a média populacional?
+#b) Que tamanho deve ter uma amostra para que o intervalo 150 ± 0,98 tenha 95% de confiança?
