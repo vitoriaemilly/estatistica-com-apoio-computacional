57df9dfb64dbe6472b61930b6c5c4b41282b5bbc
vitoria emilly <125896190+vitoriaemilly@users.noreply.github.com>
2026-06-08 20:12:27 -0300

    Implement confidence interval calculations in ap2revisao.r
    Add calculations for confidence interval and sample size

--- ap2revisao.r
+++ ap2revisao.r
@@ -2,3 +2,14 @@
 #a) Colhendo uma amostra de 36 dessas crianças, observou-se a média de 150cm. Qual o intervalo
 #de confiança de 95% para a média populacional?
 #b) Que tamanho deve ter uma amostra para que o intervalo 150 ± 0,98 tenha 95% de confiança?
+
+desvio <-5
+confianca <-0.95
+
+n <-36
+media_a <-150
+
+#a)
+alpha <- 1 - confianca
+
+z<-qnorm(1-alfa/2)
