bafc85f44daf0a5a55e380e94456a705433cf939
vitoria emilly <125896190+vitoriaemilly@users.noreply.github.com>
2026-06-09 19:29:41 -0300

    Add hypothesis testing using t-test in ap2q4.r

--- ap2q4.r
+++ ap2q4.r
@@ -7,6 +7,7 @@
 #Usando um teste de hipótese a 1% de significância nós
 #podemos rejeitar a hipótese nula?
 # 1. Atualizando com os dados REAIS da Questão 4
+
 dados <- c(1230, 582, 576, 2093, 2621, 1045, 1439, 717, 1838, 1359)
 media_estado <- 1229
 significancia <- 0.01
@@ -14,3 +15,4 @@ significancia <- 0.01
 # 2. Rodando o teste t correto
 resultado <- t.test(dados, mu = media_estado, alternative = "greater")
 resultado
+#teste de hipóteses
