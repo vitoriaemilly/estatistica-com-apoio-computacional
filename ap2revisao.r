f0d8146fc409704055e1e4598290cac75176fce0
vitoria emilly <125896190+vitoriaemilly@users.noreply.github.com>
2026-06-09 15:18:13 -0300

    Update confidence interval calculations in ap2revisao.r
    Refactor confidence interval calculations and improve comments.

--- ap2revisao.r
+++ ap2revisao.r
@@ -3,21 +3,41 @@
 #de confiança de 95% para a média populacional?
 #b) Que tamanho deve ter uma amostra para que o intervalo 150 ± 0,98 tenha 95% de confiança?
 
+#a)Colhendo uma amostra de 36 dessas crianças, 
+#observou-se a média de 150cm. Qual o intervalo
+#de confiança de 95% para a média populacional?
+
 desvio <-5
 confianca <-0.95
 
 n <-36
 media_a <-150
 
+#do total
+erro_padrao<- desvio/ (sqrt(n))
+erro_padrao
+
 #valor de z para 95% de confiança
-alpha <- 1 - confianca
+#1 menos confiança tem o valor do erro das caudas
+alfa <- 1 - confianca #0.05
 z<-qnorm(1-alfa/2)
 
-# o erro padrão da média
-erro_padrao <- desvio / sqrt(n)
-
-#erros
 margem_erro <- z * erro_padrao
 
-lim_superior<-media_a + margem_erro
-lim_inferiror<-media_a - margem_erro
+lim_in <- media_a - margem_erro
+lim_sup <- media_a + margem_erro
+
+# imprime
+cat("O intervalo de confiança para 95% é de [", round(lim_in, 2), ";", round(lim_sup, 2), "]\n")
+
+#b) e tamanho deve ter uma amostra para 
+#que o intervalo 150 ± 0,98 tenha 95% de confiança?
+
+erro_tolerado <- 0.98
+#tamanho da amostra
+tamanho <- (z * desvio /erro_tolerado)^2
+
+#arredonda p cima
+n_final <- ceiling(tamanho)
+
+n_final
