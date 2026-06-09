daa1cb48541f0ba6b7f859564d4bf59948749130
vitoria emilly <125896190+vitoriaemilly@users.noreply.github.com>
2026-06-09 16:25:48 -0300

    Create ap2q4.r

---
+++ ap2q4.r
@@ -0,0 +1,16 @@
+#Estamos desconfiados de que a média das receitas municipais per 
+#capita das cidades pequenas (0 –20.000 habitantes) é maior do que a das 
+#receitas do estado, que é de 1.229 unidades. Para comprovar
+#ou não essa hipótese, sorteamos dez cidades pequenas, 
+#e obtivemos os seguintes resultados: 1230; 582;
+#576; 2093; 2621; 1045; 1439; 717; 1838; 1359.
+#Usando um teste de hipótese a 1% de significância nós
+#podemos rejeitar a hipótese nula?
+# 1. Atualizando com os dados REAIS da Questão 4
+dados <- c(1230, 582, 576, 2093, 2621, 1045, 1439, 717, 1838, 1359)
+media_estado <- 1229
+significancia <- 0.01
+
+# 2. Rodando o teste t correto
+resultado <- t.test(dados, mu = media_estado, alternative = "greater")
+resultado
